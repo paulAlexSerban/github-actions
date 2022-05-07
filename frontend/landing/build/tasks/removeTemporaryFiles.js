f57b378f0c24ff80b83a985d263ece19d26c4e0f
paulAlexSerban <paul.alex.serban@gmail.com>
2022-05-07 23:49:36 +0300

    feat(ci): update structure and ci scripts

--- frontend/landing/build/tasks/removeTemporaryFiles.js
+++ frontend/landing/build/tasks/removeTemporaryFiles.js
@@ -1,7 +1,6 @@
 import del from "del";
 import { paths } from "../config/paths";
-import { PROJECT_PATH } from "../config/constants";
 
 export const removeTemporaryFiles = () => {
-  return del([`${paths.dist.distDir}/${PROJECT_PATH}/styles/.tmp`]);
+  return del([`${paths.dist.distDir}/public/styles/.tmp`]);
 };
